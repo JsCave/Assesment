0d666c8d895856a885c0b816635086f744422405
JsCave <67611356+JsCave@users.noreply.github.com>
2021-10-01 03:49:12 +0200

    added cors

--- router_api.js
+++ router_api.js
@@ -4,6 +4,9 @@ const usercontrol=require("./controllers/userControl")
 const cartcontrol=require("./controllers/cartControl")
 const admincontrol=require("./controllers/adminControl")
 
+const cors=require('cors')
+apiRouter.use(cors())
+
 apiRouter.post('/login',usercontrol.apiLogin)
 apiRouter.get('/order/:id',usercontrol.apiMustBeLoggedIn,cartcontrol.apiOrder)
 module.exports=apiRouter
